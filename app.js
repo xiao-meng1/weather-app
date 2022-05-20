603c1035455b40abaa25854f9834d034faa58586
Xiao <xmeng1@ualberta.ca>
2022-05-20 12:30:19 -0600

    add function for processing response data

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const getWeatherData = async (city) => {
   const apiKey = 'a424ef2f751e5afc06fa27463584df1d';
-  const apiCallString = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  const apiCallString = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   const response = await fetch(apiCallString, {mode: 'cors'});
-  const data = await response.json();
+  const data = response.json();
+  console.log(data)
   return data;
 };
 
-const processWeatherData = () => {
-
+const processWeatherData = (data) => {
+  console.log(data)
+  const city = data.name;
+  const country = data.sys.country;
+  const temperature = data.main.temp;
+  const feelsLikeTemperature = data.main.feels_like;
+  const wind = data.wind.speed;
+  const humidity = data.main.humidity;
+  
+  return {city, 
+          country, 
+          temperature, 
+          feelsLikeTemperature, 
+          wind, 
+          humidity};
 }
 
-const data = getWeatherData('Edmonton')
+
+(async () => {
+  const data = await getWeatherData('Guangzhou');
+  const processedData = processWeatherData(data);
+  console.log(processedData)
+})();
